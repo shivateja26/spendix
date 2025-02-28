1fcf4a64e999593cd274ea64bbf343142c9787e4
Amalakanti Shiva teja <shivateja.amalakanti@gmail.com>
2025-02-28 14:04:21 +0530

    MongoDB Connection

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
-require("dotenv").config;
+require("dotenv").config(); 
 const express = require("express");
-const cors = ("cors");
-const path = require(path);
+const { connect } = require("mongoose");
+const cors = require("cors"); 
+const path = require("path");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-//middleware to hande cors
+
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods:["GET","POST","PUT","DELETE"],
-        allowedHeaders:["Content-Type","Authorization"],
+        origin: process.env.CLIENT_URL || "*",  
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
-app.use(express.json());
+app.use(express.json());  
 
+connectDB();  
+app.use("api/v1/auth",authRoutes);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`Server running on pn port ${PORT}`))
-
-
-
-
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
