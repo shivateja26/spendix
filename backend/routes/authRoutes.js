d40efe2dbbb4705434b21cac5412b5f3adb940c5
Amalakanti Shiva teja <shivateja.amalakanti@gmail.com>
2025-03-14 12:04:47 +0530

    Changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,6 +6,7 @@ const {
     loginUser,
     getUserInfo,
 } = require("../controllers/authController");
+
 const upload = require("../middleware/uploadMiddleware");
 
 const router = express.Router();
@@ -13,7 +14,7 @@ const router = express.Router();
 router.post("/register", registerUser);  
 router.post("/login", loginUser);        
 router.get("/getUser", protect, getUserInfo);
-router.post("/upload-imge",upload.single("image"),(req,res)=>{
+router.post("/upload-image",upload.single("image"),(req,res)=>{
     if(!req.file){
         return res.status(400).json({message:"No file uploaded"});
     }
